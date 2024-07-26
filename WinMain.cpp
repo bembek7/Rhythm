a99bc10c601079225b77182f4298e6662ac77ad9
bpucilowski <pucilowskibartlomiej@gmail.com>
2024-07-26 21:24:54 +0200

    Add basic graphics presenting solid color.

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,5 +1,8 @@
 #include "Windows.h"
 #include <system_error>
+#include <wrl.h>
+#include <dxgi.h>
+#include <d3d11.h>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -15,6 +18,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR pCmdLine, int nCmdShow)
 {
 	const char winClassName[] = "RythmWindow";
+	const int windowWidth = 800;
+	const int windowHeight = 600;
 
 	WNDCLASSEXA windowClass = {};
 	windowClass.cbSize = sizeof(WNDCLASSEX);
@@ -31,8 +36,8 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR pCmdLi
 		WS_OVERLAPPEDWINDOW,
 		100,
 		100,
-		800,
-		640,
+		windowWidth,
+		windowHeight,
 		nullptr,
 		nullptr,
 		hInstance,
@@ -46,10 +51,69 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR pCmdLi
 
 	ShowWindow(hWnd, nCmdShow);
 
+	Microsoft::WRL::ComPtr<IDXGISwapChain> pSwapChain;
+	Microsoft::WRL::ComPtr<ID3D11Device> pDevice;
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> pContext;
+	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> pRenderTargetView;
+
+	DXGI_SWAP_CHAIN_DESC scd = {};
+	scd.BufferDesc.Width = 0;
+	scd.BufferDesc.Height = 0;
+	scd.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+	scd.BufferDesc.RefreshRate.Numerator = 0;
+	scd.BufferDesc.RefreshRate.Denominator = 0;
+	scd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	scd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	scd.SampleDesc.Count = 1;
+	scd.SampleDesc.Quality = 0;
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	scd.BufferCount = 1;
+	scd.OutputWindow = hWnd;
+	scd.Windowed = TRUE;
+	scd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	scd.Flags = 0;
+
+	UINT swapCreateFlags = 0u;
+#ifndef NDEBUG
+	swapCreateFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	HRESULT hr = D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		swapCreateFlags,
+		nullptr,
+		0,
+		D3D11_SDK_VERSION,
+		&scd,
+		&pSwapChain,
+		&pDevice,
+		nullptr,
+		&pContext
+	);
+
+	Microsoft::WRL::ComPtr<ID3D11Resource> pBackBuffer;
+	pSwapChain->GetBuffer(0, __uuidof(ID3D11Resource), &pBackBuffer);
+	pDevice->CreateRenderTargetView(pBackBuffer.Get(), nullptr, &pRenderTargetView);
+
+
+	pContext->OMSetRenderTargets(1u, pRenderTargetView.GetAddressOf(), nullptr);
+
+	D3D11_VIEWPORT viewport = {};
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = windowWidth;
+	viewport.Height = windowHeight;
+	viewport.MaxDepth = 1.f;
+	viewport.MinDepth = 0.f;
+
+	pContext->RSSetViewports(1u, &viewport);
+
 	MSG msg = { };
 	while (true)
 	{
-		while (PeekMessageA(&msg, NULL, 0, 0, true))
+		while (PeekMessageA(&msg, NULL, 0u, 0u, true))
 		{
 			if (msg.message == WM_QUIT)
 			{
@@ -61,6 +125,9 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR pCmdLi
 		}
 
 		// Graphics here
+		const float color[] = { 1.0f, 0.f, 0.f, 1.0f };
+		pContext->ClearRenderTargetView(pRenderTargetView.Get(), color);
+		pSwapChain->Present(1u, 0u);
 	}
 	
 	return 0;
